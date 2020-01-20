26e34e51c434ff004092bb8ab9b0cf58d7874adc
Joe <46511897+jkusita@users.noreply.github.com>
2020-01-20 10:17:32 +0800

    Update not-now.py

--- not-now.py
+++ not-now.py
@@ -1,18 +1,18 @@
 # Makes a list of things you shouldn't search up right now.
 import os
-
+folder_path = ""
 while True:
     # Asks what the user wants to do.
     user_decision = input("Do you want to read, append, or stop?: ")
 
     if user_decision == "read":
-        not_now_file = open("/Users/ramteechua/Desktop/Files/project-not-now/not-now.txt", "r")
+        not_now_file = open(folder_path, "r")
         contents = not_now_file.read()
         print("\n" + contents)
         not_now_file.close()
     elif user_decision == "append":
         words = input("What do you want to write?: ")
-        not_now_file = open("/Users/ramteechua/Desktop/Files/project-not-now/not-now.txt", "a")
+        not_now_file = open(folder_path, "a")
         not_now_file.write("• " + words + " \n")
         not_now_file.close()
     elif user_decision == "stop":
